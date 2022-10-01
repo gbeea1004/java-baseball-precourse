f9701900658f7b9618478d90e8480975d1ee5378
my <jvckigh@gmail.com>
2022-10-02 00:34:59 +0900

    refactor: enum 리스트를 map 으로 저장해서 조회하도록 수정

--- src/main/java/baseball/domain/GameState.java
+++ src/main/java/baseball/domain/GameState.java
@@ -2,12 +2,21 @@ package baseball.domain;
 
 import baseball.exception.BaseballGameException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum GameState {
 
     PROGRESS("1"),
     END("2"),
     ;
 
+    private static final Map<String, GameState> gameStates = new HashMap<>();
+    static {
+        for (GameState gameState : values()) {
+            gameStates.put(gameState.state, gameState);
+        }
+    }
     private final String state;
 
     GameState(String state) {
@@ -19,12 +28,10 @@ public enum GameState {
     }
 
     public static GameState getGameState(String state) {
-        // TODO: depth 2 분리하기
-        for (GameState gameState : values()) {
-            if (gameState.state.equals(state)) {
-                return gameState;
-            }
+        GameState gameState = gameStates.get(state);
+        if (gameState == null) {
+            throw new BaseballGameException("게임 상태를 잘못 입력하셨습니다.");
         }
-        throw new BaseballGameException("게임 상태를 잘못 입력하셨습니다.");
+        return gameState;
     }
 }
