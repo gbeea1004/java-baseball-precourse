0107f8cfc99fe451162a7719c56e521d601a1e8f
my <jvckigh@gmail.com>
2022-10-02 00:56:30 +0900

    feat: AppConfig 추가하여 Config 를 통해 Controller 가져오도록 수정
    - 객체 생성을 AppConfig 가 담당하도록 역할 위임

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,12 @@
 package baseball;
 
+import baseball.config.AppConfig;
 import baseball.controller.BaseballGameController;
-import baseball.service.BaseballGameService;
 
 public class Application {
 
     public static void main(String[] args) {
-        BaseballGameController baseballGameController = new BaseballGameController(new BaseballGameService());
+        BaseballGameController baseballGameController = AppConfig.baseballGameController();
         baseballGameController.run();
     }
 }
